ac1316c2769401ecc7a5775f8dac1d5f96d352a1
joyceyskwon <joyceyskwon@gmail.com>
2019-05-17 15:54:08 -0400

    first commit

---
+++ src/components/BreweriesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BreweriesContainer extends Component {
+    render() {
+        return (
+            <div>
+                "inside container"
+            </div>
+        )
+    }
+}
+
+export default BreweriesContainer
