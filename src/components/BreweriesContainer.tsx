c0a2ac986628d652e886e864bb69367a1e1185de
joyceyskwon <joyceyskwon@gmail.com>
2019-05-19 04:09:02 -0400

    added code to container and adapter

--- src/components/BreweriesContainer.tsx
+++ src/components/BreweriesContainer.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import BreweryItem from './BreweryItem'
+import adapter from '../services/adapter'
 
 class BreweriesContainer {
-  
+  state = {
+    breweries: [],
+    breweryClicked: false,
+    clickedBot: null
+  }
+
+  componentDidMount() {
+    adapter.fetchBreweries()
+      .then(breweries => {
+        if (breweries) {
+          this.setState({ breweries })
+        } else {
+          return this.state.breweries
+        }
+      })
+  }
+
+  findBreweryById = breweryId => this.state.breweries.find(b => b.id === breweryId)
+
+  handleClick = breweryId => {
+    this.setState({
+      breweryClicked: true,
+      clickedBot: this.findBreweryById(breweryId)
+    })
+  }
+
+
+
+  mapBreweries = () => {
+    return this.state.breweries.map(brewery => {
+      return <BreweryItem
+        key={brewery.id}
+        brewery={brewery}
+        handleClick={this.handleClick}
+      />
+    })
+  }
+
   render() {
     return (
-
+      <div className="ui four column grid">
+        <div className="row">
+          {this.mapBreweries()}
+        </div>
+      </div>
     )
   }
 }
---
+++ src/services/adapter.tsx
@@ -0,0 +1,13 @@
+const API_URL = 'https://breweries-app-server.herokuapp.com/api/v1/breweries'
+
+// const headers = {
+//     'Content-Type': 'application/json',
+//     'Accepts': 'application/json'
+// }
+
+const fetchBreweries = () => {
+  return fetch(API_URL)
+    .then(res => res.json())
+}
+
+export default { fetchBreweries }
